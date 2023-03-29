3508e62a567e5cfa4feb5c6a77e18063284ba0e4
Oliver Liou Yu You <128446044+yyliou@users.noreply.github.com>
2023-03-29 13:56:38 +0800

    Update example.R

--- example.R
+++ example.R
@@ -1,7 +1,4 @@
 # pkg required
-
-library("geosphere")
-library("XML")
 library("readODS")
 library("dplyr")
 library("reshape2")
@@ -10,19 +7,10 @@ library("ggplot2")
 library("magrittr")
 
 # example
-m <- mrt(begin    = 201501,
-         end      = 201509,
-         location = "/Users/liuyuyou/Desktop/MRT",
-         radius   = 2)
-
-# pure data
-b <- mrtp(begin    = 201501,
+b <- mrtp(begin    = 202101,
           end      = 202212,
           location = "/Users/oliverliou/Desktop")
 b <- b[b$date == "2022-09-03",]
 
 # plotting
-plot(b, out = T) # 想看建站或出站
-
-g <- b[b$station_name == "公館"  & b$type == 1,]
-write.csv(g,"/Users/liuyuyou/Desktop/MRT/gon.csv")
+plot(b, out = T)
