9b73b2be78be2b3dc9177f8287aca3017119feec
Oliver Liou Yu You <128446044+yyliou@users.noreply.github.com>
2023-03-30 12:06:32 +0800

    Update example.R

--- example.R
+++ example.R
@@ -5,11 +5,16 @@ library("reshape2")
 library("data.table")
 library("ggplot2")
 library("magrittr")
+library("hydroTSM")
 
-# example
-b <- mrtp(begin    = 202101,
-          end      = 202212,
-          location = "/Users/oliverliou/Desktop")
+# generate station-date data
+b <- mrtp(begin = 202101, end = 202212, location = "/Users/oliverliou/Desktop")
 
 # plotting
 plot(b, out = T)
+
+# generate a time series data
+c <- td(b)
+
+# forecasting by a fixed proportion
+pd(c,.9)
