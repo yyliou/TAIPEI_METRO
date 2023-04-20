e02b95194c5a83f300062cf36878f5e774b5f2d5
Oliver Liou Yu You <128446044+yyliou@users.noreply.github.com>
2023-04-20 13:07:37 +0800

    Update example.R

--- example.R
+++ example.R
@@ -8,7 +8,16 @@ library("magrittr")
 library("hydroTSM")
 
 # generate station-date data
-b <- mrtp(begin = 202101, end = 202212, location = "/Users/oliverliou/Desktop")
+b <- mrtp(begin = 201801, end = 202212, location = "/Users/oliverliou/Desktop")
+
+ls(b)
+test <- b[b$type == 1,]
+test$lnf <- log(test$flow)
+model <- lm(lnf ~ year + season + month + week + day + ny + cny +
+              branch + branch_trans + high +
+              hot + lrt + main_trans + node + rail +
+              covid_2 + covid_3 + ticket, data = test)
+summary(model)
 
 # plotting
 plot(b)
