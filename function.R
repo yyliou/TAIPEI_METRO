7befab4873a57bc6773f739563aed1f9b6c1de40
Oliver Liou Yu You <128446044+yyliou@users.noreply.github.com>
2023-04-20 13:00:04 +0800

    Update function.R

--- function.R
+++ function.R
@@ -28,6 +28,8 @@ mrtp <- function(begin,end,location){
   data$week <- lubridate::wday(data$date)
   data$weekend[data$week < 7 & data$week > 1] <- 0
   data$weekend[data$week == 1 | data$week == 7] <- 1
+  data$ny <- ifelse(data$month*100 + data$day == 101 |
+                      data$month*100 + data$day == 1231,1,0)
   data$season <- factor(hydroTSM::time2season(data$date, out.fmt = "seasons"))
   data$year <- factor(data$year)
   data$season <- factor(data$season) 
@@ -119,6 +121,7 @@ mrtp <- function(begin,end,location){
                            data$date < "2021-05-16"),1,0)
   data$covid_3 <- ifelse(data$date > "2021-05-15"  & 
                            data$date < "2022-02-28",1,0)
+  data$ticket <- ifelse(data$date > "2018-04-15", 1, 0)
   data(holiday)
   cny <- data.frame(date = cny,
                     cny = 1)
@@ -157,8 +160,6 @@ plot <- function(object){
 
 # 3. transfer to a time series data from 1
 td <- function(object){
-  object$year <- as.numeric(as.character(object$year))
-  object$month <- as.numeric(as.character(object$month))
   object[object$type == 2]  %>% 
     dplyr::group_by(date) %>%
     dplyr::summarise(flow = sum(flow, na.rm = TRUE)/1000000)
@@ -172,13 +173,39 @@ pd <- function(object, porportion = .9){
   object$week <- lubridate::wday(object$date)
   object$day <- lubridate::day(object$date)
   object$weekend <- ifelse(object$week==1|object$week==7, 1, 0)
+  object$ny <- ifelse(object$month*100 + object$day == 101 |
+                        object$month*100 + object$day == 1231,1,0)
   object$year <- factor(object$year)
   object$month <- factor(object$month)  
   object$week <- factor(object$week)  
   object$day <- factor(object$day)  
   object$season <- factor(hydroTSM::time2season(object$date, out.fmt = "seasons"))
+  data(holiday)
+  cny <- data.frame(date = cny,
+                    cny = 1)
+  cny_1 <- cny
+  cny_0 <- cny
+  cny_0$date <- cny_0$date - 1
+  cny_2 <- cny
+  cny_2$date <- cny_2$date + 1
+  cny_3 <- cny
+  cny_3$date <- cny_3$date + 2
+  cny_4 <- cny
+  cny_4$date <- cny_4$date + 3
+  cny <- rbind(cny_0,cny_1)
+  cny <- rbind(cny,cny_2)
+  cny <- rbind(cny,cny_3)
+  cny <- rbind(cny,cny_4)
+  object <- left_join(object,cny,by = "date")
+  object$cny[is.na(object$cny)] <- 0
+  object$covid_2 <- ifelse((object$date > "2021-05-10"  & 
+                              object$date < "2021-05-16"),1,0)
+  object$covid_3 <- ifelse(object$date > "2021-05-15"  & 
+                             object$date < "2022-02-28",1,0)
+  object$ticket <- ifelse(object$date > "2018-04-15", 1, 0)
   train <- object[object$N < length(object$flow)*porportion,]
-  model <- lm(flow ~ N + season + month + weekend + day, data = train)
+  model <- lm(flow ~ N + season + month + weekend + ny +
+                cny + covid_2 + covid_3 + ticket, data = train)
   summary(model)
   R2 <- paste0("ex-ante R2 =",round(summary(model)$r.squared,3))
   object$pred <- predict(model, newdata = object)
